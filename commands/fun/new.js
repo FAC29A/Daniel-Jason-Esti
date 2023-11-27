d1e50ce3c3ea0ad8799381af55a2ab026eb7b288
Esti <stsh7573@gmail.com>
2023-11-27 20:46:10 +0000

    Update new.js

--- commands/fun/new.js
+++ commands/fun/new.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("Hello")
+    .setName("hello")
     .setDescription("Replies with שלום!"),
   async execute(interaction) {
     await interaction.reply("!שלום");
