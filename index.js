88556dbf5edf1a11d669b5b74a054e715342af36
Jason Warren <contact.jwarren@gmail.com>
2023-11-28 15:57:59 +0000

    Language Packages (#2)
    Tracery: System grammar created
    Tracery: imported to index.js
    Compromise: imported to index.js
    Other: Languages folder

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
+// LANGUAGE NONSENSE
+const tracery = require('tracery-grammar');
+const grammar = require('./language/grammar.js');
+const nlp = require('compromise');
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
