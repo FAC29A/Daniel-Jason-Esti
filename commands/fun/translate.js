4ec96805d13754e6229e25ae7b44c8a112ff1a37
Esti <stsh7573@gmail.com>
2023-11-29 21:15:17 +0000

    API set up

--- commands/fun/translate.js
+++ commands/fun/translate.js
@@ -1,25 +1,43 @@
-const translate = require("google-translate-api");
-
+const { SlashCommandBuilder } = require("discord.js");
+const MsTranslator = require("mstranslator");
+const { apikey } = require("../../config.json");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("translate")
     .setDescription("Translate text to another language")
     .addStringOption((option) =>
-      option.setName("text").setDescription("Enter text to translate").setRequired(true)
+      option
+        .setName("text")
+        .setDescription("Enter text to translate")
+        .setRequired(true)
     )
     .addStringOption((option) =>
-      option.setName("language").setDescription("Enter target language code").setRequired(true)
+      option
+        .setName("language")
+        .setDescription("Enter target language code")
+        .setRequired(true)
     ),
   async execute(interaction) {
     const textToTranslate = interaction.options.getString("text");
     const targetLanguage = interaction.options.getString("language");
 
-    try {
-      const translation = await translate(textToTranslate, { to: targetLanguage });
-      await interaction.reply(`Translated: ${translation.text}`);
-    } catch (error) {
-      console.error(error);
-      await interaction.reply("Translation failed. Please try again.");
-    }
+    const client = new MsTranslator({
+      api_key: apikey, // Replace with your actual API key
+    });
+
+    client.translate(
+      {
+        text: textToTranslate,
+        to: targetLanguage,
+      },
+      (err, translation) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(`Original: ${textToTranslate}`);
+          console.log(`Translation: ${translation}`);
+        }
+      }
+    );
   },
 };
