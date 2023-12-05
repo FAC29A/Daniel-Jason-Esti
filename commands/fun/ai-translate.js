6e68ebc6ff1f9d700f63da444d1275cd2f4035b5
Esti <stsh7573@gmail.com>
2023-12-05 13:48:15 +0000

    Update ai-translate.js

--- commands/fun/ai-translate.js
+++ commands/fun/ai-translate.js
@@ -2,11 +2,9 @@ const apiKey = "CiA5CJYu0d7lRSrteot5T3BlbkFJnD5QpQLbZMkh43J06vbX";
 const sourceText =
   "Please translate this exact phrase into Hebrew. making sure you keep the same tone & nuance as the original message";
 const { SlashCommandBuilder } = require("discord.js");
-
-// Endpoint for translation API (replace with the actual endpoint provided by the API provider)
+const { MsTranslator } = require("mstranslator");
 const apiUrl = "https://api.example.com/translate";
 
-// Function to translate text using the AI API
 async function translateText(apiKey, text) {
   const response = await fetch(apiUrl, {
     method: "POST",
@@ -16,7 +14,7 @@ async function translateText(apiKey, text) {
     },
     body: JSON.stringify({
       text,
-      targetLanguage: "he", // 'he' is the language code for Hebrew
+      targetLanguage: "he",
     }),
   });
 
@@ -25,7 +23,7 @@ async function translateText(apiKey, text) {
   }
 
   const result = await response.json();
-  return result.translatedText;
+  return result[0].translations[0].text;
 }
 
 module.exports = {
@@ -49,12 +47,10 @@ module.exports = {
     const targetLanguage = interaction.options.getString("language");
 
     const client = new MsTranslator({
-      api_key: apikey, // Replace with your actual API key
+      api_key: apiKey,
     });
+
+    const translatedText = await translateText(apiKey, textToTranslate);
+    console.log(`Translated Text: ${translatedText}`);
   },
 };
-
-// Translate the text
-translateText(apiKey, sourceText)
-  .then((translatedText) => console.log(`Translated Text: ${translatedText}`))
-  .catch((error) => console.error(`Error: ${error.message}`));
