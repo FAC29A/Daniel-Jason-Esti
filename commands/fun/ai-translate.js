8aee5ed1c4e495d937a96ab10674fb163579bb79
Esti <stsh7573@gmail.com>
2023-12-05 13:44:33 +0000

    Update ai-translate.js

--- commands/fun/ai-translate.js
+++ commands/fun/ai-translate.js
@@ -28,6 +28,32 @@ async function translateText(apiKey, text) {
   return result.translatedText;
 }
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ai-translate")
+    .setDescription("Translate text to another language")
+    .addStringOption((option) =>
+      option
+        .setName("text")
+        .setDescription("Enter text to translate")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("language")
+        .setDescription("Enter target language code")
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    const textToTranslate = interaction.options.getString("text");
+    const targetLanguage = interaction.options.getString("language");
+
+    const client = new MsTranslator({
+      api_key: apikey, // Replace with your actual API key
+    });
+  },
+};
+
 // Translate the text
 translateText(apiKey, sourceText)
   .then((translatedText) => console.log(`Translated Text: ${translatedText}`))
